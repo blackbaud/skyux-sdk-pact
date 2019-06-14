3355d3789c812d11ae6ac17ee11d6fe7ed53e80c
Paul Crowder <paul.crowder@blackbaud.com>
2019-06-14 11:21:52 -0400

    Added `getContextToken` method to mock provider (#10)
    Closes #9

--- src/app/public/modules/pact/pact-auth-token-provider.ts
+++ src/app/public/modules/pact/pact-auth-token-provider.ts
@@ -11,4 +11,8 @@ export class SkyPactAuthTokenProvider {
   public getToken(args?: BBAuthGetTokenArgs): Promise<string> {
     return Promise.resolve('mock_access_token_auth-client@blackbaud.com');
   }
+
+  public getContextToken(args?: BBAuthGetTokenArgs): Promise<string> {
+    return this.getToken(args);
+  }
 }
