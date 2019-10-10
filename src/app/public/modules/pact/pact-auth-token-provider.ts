d505c1ae5a2f2a2c2b9098692dfc9c66be0fee6f
Blackbaud-KrisMahon <Kris.Mahon@blackbaud.com>
2019-10-10 09:09:05 -0400

    Added missing `decodeToken` method to token provider. (#16)

--- src/app/public/modules/pact/pact-auth-token-provider.ts
+++ src/app/public/modules/pact/pact-auth-token-provider.ts
@@ -6,13 +6,33 @@ import {
   BBAuthGetTokenArgs
 } from '@blackbaud/auth-client';
 
+import {
+  SkyAuthToken,
+  SkyAuthTokenContextArgs
+} from '@skyux/http';
+
 @Injectable()
 export class SkyPactAuthTokenProvider {
   public getToken(args?: BBAuthGetTokenArgs): Promise<string> {
     return Promise.resolve('mock_access_token_auth-client@blackbaud.com');
   }
 
+  public getDecodedToken(args?: BBAuthGetTokenArgs): Promise<SkyAuthToken> {
+    return this.getToken(args)
+      .then((token) => {
+        return this.decodeToken(token);
+      });
+  }
+
   public getContextToken(args?: BBAuthGetTokenArgs): Promise<string> {
     return this.getToken(args);
+  }
+
+  public getDecodedContextToken(args?: SkyAuthTokenContextArgs): Promise<SkyAuthToken> {
+    return this.getDecodedToken();
+  }
+
+  public decodeToken(token: string): SkyAuthToken {
+    return {};
   }
 }
